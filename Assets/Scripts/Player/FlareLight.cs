410047bf69f93a4a487e8a955e7b1813c87174de
Adrianis <chester3000@hotmail.com>
2014-08-24 23:41:43 +0100

    Added Flare object
    basic capsule with a mesh PS
    light range attentuates over time down to min, then the light gets
    disabled, althogu the capsule stays
    the flare uses a RB for physics & is propelled by initial force from the
    spawner

---
+++ Assets/Scripts/Player/FlareLight.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlareLight : MonoBehaviour {
+
+    public float timeTilOut;
+
+    private Light light;
+    private float rangeOri;
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+        rangeOri = light.range;
+        StartCoroutine(AttenuateLight());
+    }
+
+
+    private IEnumerator AttenuateLight()
+    {
+        yield return new WaitForSeconds(timeTilOut / 2);
+
+        float rate = 1 / (timeTilOut / 2);
+        float i = 1;
+        
+        while (i > 0)
+        {
+            i -= Time.deltaTime * rate;
+            light.range = Mathf.Lerp(0, rangeOri, i);
+            yield return new WaitForEndOfFrame();
+        }
+
+        light.enabled = false;
+    }
+}
