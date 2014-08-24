7a885caaf58d18994c0c950ef2e9ccdd26dfa8de
Adrianis <chester3000@hotmail.com>
2014-08-24 21:29:56 +0100

    Added code to CameraFollow

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -3,11 +3,40 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    public float distance;
+    private GameObject PlayerCtrl;
+    public Vector3 PlayerPos;
+
+    public float Distance;
+    public float Angle;
+
+    public float ScrollLimitLower;
+    public float ScrollLimitUpper;
+
+
+    void Start()
+    {
+        PlayerCtrl = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void Update()
     {
+        PlayerCtrl = GameObject.FindGameObjectWithTag("Player");
 
+        if (Input.GetAxis("ScrollWheel") < 0)
+            Distance += 0.1f;
+        else if (Input.GetAxis("ScrollWheel") > 0)
+            Distance -= 0.1f;
+
+        if (Distance < ScrollLimitLower) Distance = ScrollLimitLower;
+        else if (Distance > ScrollLimitUpper) Distance = ScrollLimitUpper;
+
+        PlayerPos = PlayerCtrl.transform.localPosition;
+        PlayerPos.x += 0 * Distance;
+        PlayerPos.y += 14 * Distance;
+        PlayerPos.z += Angle * Distance;
+
+        transform.position = PlayerPos;
+        transform.LookAt(PlayerCtrl.transform);
     }
 
 }
