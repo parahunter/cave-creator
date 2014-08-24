9b49375b8e7e46221d15ac39b37626ff0db2fc5a
Adrianis <chester3000@hotmail.com>
2014-08-24 20:58:45 +0100

    Added simple rotation script for ship engines

---
+++ Assets/Scripts/Player/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    public float rotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, (rotationSpeed * Time.deltaTime));
+    }
+
+
+}
