83de6af6bb8668d5927b48f367543ff11d80a1d5
Adrianis <chester3000@hotmail.com>
2014-08-24 23:40:12 +0100

    Added script to fire a Flare
    uses lmb, spawns a flare and adds force to it

---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public GameObject Flare;
+    public float fireForce;
+
+    public float timeDelay;
+    private float timeSinceLast;
+
+
+    void Update()
+    {
+        timeSinceLast += Time.deltaTime;
+
+        if (timeSinceLast > timeDelay)
+        {
+            if (Input.GetButtonDown("FireFlare"))
+            {
+                GameObject NewFlare = Instantiate(Flare, transform.position, transform.rotation) as GameObject;
+                NewFlare.rigidbody.AddForce(transform.forward * fireForce);
+                timeSinceLast = 0;
+            }
+        }
+    }
+
+}
