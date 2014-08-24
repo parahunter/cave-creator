2333ba1070d7ff0dfa2a603a7c39af77f62a8cf6
Adrianis <chester3000@hotmail.com>
2014-08-24 17:53:34 +0100

    Added code to PlayerMovement for Forward/Side/Up

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    
+    public float forwardSpeedCoef;
+    public float sideSpeedCoef;
+    public float upSpeedCoef;
+
+    private float forwardSpeedOri;
+
+
+    void Start()
+    {
+        forwardSpeedOri = forwardSpeedCoef;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Afterburner"))
+            forwardSpeedCoef = forwardSpeedOri * 2;
+        else forwardSpeedCoef = forwardSpeedOri;
+
+        Vector3 MoveDir = (transform.forward * Input.GetAxis("ForwardThrust") * forwardSpeedCoef * Time.deltaTime)
+                         + (transform.right * Input.GetAxis("SideThrust") * sideSpeedCoef * Time.deltaTime)
+                         + (transform.up * Input.GetAxis("UpThrust") * upSpeedCoef * Time.deltaTime);
+        rigidbody.AddForce(MoveDir);
+    }
+
+
 }
