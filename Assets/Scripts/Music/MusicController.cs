c9d3f33d92ae753b5547a2c9a6009475fa7fe8e6
parahunter <parahunter@gmail.com>
2014-08-25 21:21:00 +0100

    redid music player

--- Assets/Scripts/Music/MusicController.cs
+++ Assets/Scripts/Music/MusicController.cs
@@ -8,7 +8,7 @@ public class MusicController : MonoBehaviour
 	AudioClip clipBeingPlayed;
 	
 	AudioSource[] sources;
-	int activeClip = 0;
+	int activeSource = 0;
 				
 	public float fadeTime = 2f;			
 	public float volume = 0.5f;			
@@ -20,11 +20,15 @@ public class MusicController : MonoBehaviour
 		private set;
 	}
 				
+	bool activated = false;			
+				
 	// Use this for initialization
 	void Awake () 
 	{
-		if(instance == null)
+		if(instance == null )
 		{
+			print ("made new one");
+			activated = true;
 			instance = this;
 			DontDestroyOnLoad(gameObject);
 			sources = new AudioSource[2];
@@ -33,25 +37,58 @@ public class MusicController : MonoBehaviour
 						
 			Play ();
 		}
-		else
+		else if(instance != this)
+		{
+			print ("destroyed");
 			Destroy(gameObject);
-		
+		}
 	}
 		
 	void Play()
 	{
+		StopAllCoroutines();
 		StartCoroutine(Crossfade());
 	}
-		
+	
+	void OnLevelWasLoaded()
+	{
+		if(activated)
+			Play ();
+	}
+	
 	IEnumerator Crossfade()
 	{
+		activeSource++;
+		AudioClip clip = clips[ Random.Range(0, clips.Length)];
 		
-		while(true)
+		float accTime = 0;
+		
+		if(clipBeingPlayed != clip)
 		{
-			activeClip++;
-			AudioClip clip = clips[ Random.Range(0, clips.Length -1)];
+			float f = 0;
 			
+			clipBeingPlayed = clip;
+			//sources[activeSource % sources.Length].time = Random.Range(0, clip.length);
+			sources[activeSource % sources.Length].clip = clip;
 			
+			sources[activeSource % sources.Length].Play();
+			
+			while(f <= 1f)
+			{
+				float fadeValue = fadeCurve.Evaluate(f); 
+				sources[(activeSource) % sources.Length].volume = volume * fadeValue;
+				sources[(activeSource  + 1) % sources.Length].volume = volume * (1 - fadeValue);
+			
+				f += Time.deltaTime / fadeTime;
+				accTime += Time.deltaTime;
+				yield return 0;
+			}
 		}
+
+		print (accTime);
+
+		sources[activeSource % sources.Length].volume = volume;
+		
+		yield return 0;
 	}
 }
